a4bddf4e1cd059064eb93f5c039967f946ba38d0
pewt1403 <p_ngamboontham@hotmail.com>
2020-05-03 23:47:41 +0700

    Add faceDetector and Socket

---
+++ Final-Indiv-Project/Assets/Scripts/faceDetectorSocketServer/faceDetectorSocketServer/Program.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace faceDetectorSocketServer
+{
+    class Program
+    {
+        public static string dataIn = null;
+
+        public static void StartListening()
+        {
+            // Incoming data
+            byte[] bytes = new Byte[1024];
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName()); //Name of host running application
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7321);
+
+            //Create TCP Socket
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    Socket handler = listener.Accept();
+                    dataIn = null;
+                    if (handler.Connected)
+                    {
+                        Console.WriteLine("Connected");
+                    }
+
+                    while (true)
+                    {
+
+                        //byte[] data = new byte[handler.Available];
+                        int byteRec = handler.Receive(bytes, 0, bytes.Length, 0);
+                        Array.Resize(ref bytes, byteRec);
+                        //Console.WriteLine(Encoding.UTF8.GetString(bytes));
+                        dataIn += Encoding.UTF8.GetString(bytes);
+                        if (dataIn.IndexOf("X") > -1)
+                        {
+                            Console.WriteLine(dataIn);
+                            dataIn = null;
+
+                        }
+
+                    }
+
+                    //Console.WriteLine("Text received : {0}", dataIn);
+                    // Echo the data back to the client.  
+                    //byte[] msg = Encoding.ASCII.GetBytes(dataIn);
+
+                    //handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+            StartListening();
+            
+
+
+
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
---
+++ Python-Script/faceDetector.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import dlib
+import socket
+
+def faceDetect():
+    cap = cv2.VideoCapture(0)
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    HOST = "127.0.0.1"
+    PORT = 7321
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST,PORT))
+        s.sendall(bytes("wpw",'UTF-8'))
+        #data = s.recv(1024)
+        while True:
+            _, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Gray for detection : use less power
+
+            faces = detector(gray) # get Rectangle (face coordinate) [(.,.) (.,.)]
+            for face in faces:
+                x1 = face.left()
+                y1 = face.top()
+                x2 = face.right()
+                y2 = face.bottom()
+                #cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 3)
+    
+                landmarks = predictor(gray, face)
+                #print(landmarks)
+                dataOutX = str(landmarks.part(28).x)
+                dataOutY =  str(landmarks.part(28).y)
+                for i in range(29,68):
+
+                    x = landmarks.part(i).x
+                    dataOutX += ',' + str(landmarks.part(i).x)
+                    y = landmarks.part(i).y
+                    dataOutY += ',' + str(landmarks.part(i).y)
+                    #print(x,y)
+                    cv2.circle(frame,(x,y), 3, (255,0,0), -1)
+                dataOut = dataOutX + "/" + dataOutY + "X"
+                print(dataOut)
+                s.sendall(bytes(dataOut,'UTF-8'))
+
+            cv2.imshow("Frame", frame)
+
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+
+faceDetect()
+#print(repr(data))
