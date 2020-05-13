b4378b20804b618ac67290788c61e4ff31f6d897
nackalalalong <nak-online@hotmail.com>
2020-05-13 22:24:39 +0700

    add animationController chnage to player

--- Final-Indiv-Project/Assets/Scripts/MyFirstPersonController.cs
+++ Final-Indiv-Project/Assets/Scripts/MyFirstPersonController.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class MyFirstPersonController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Animator animator;
+    public RuntimeAnimatorController animatorControllerIdle;
+    public RuntimeAnimatorController animatorControllerWalk;
+    public RuntimeAnimatorController animatorControllerRun;
+    public RuntimeAnimatorController animatorControllerJump;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if ( Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)){
+            animator.runtimeAnimatorController = animatorControllerWalk;
+        }
+        else if ( Input.GetKey(KeyCode.LeftShift) ){
+            animator.runtimeAnimatorController = animatorControllerRun;
+        }
+        else if ( Input.GetKey(KeyCode.Space) ){
+            animator.runtimeAnimatorController = animatorControllerJump;
+        }
+        else {
+            animator.runtimeAnimatorController = animatorControllerIdle;
+        }
+
         
+
     }
 }
